dd85ba97e1ab1d3af9332450dfb4855186ce12e6
zakiguedi <zaki.guedi@gmail.com>
2023-02-08 10:01:16 -0500

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
