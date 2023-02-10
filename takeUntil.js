9c0032baaa366228a571fc57787e0b241f3fceaf
zakiguedi <zaki.guedi@gmail.com>
2023-02-09 19:03:58 -0500

    Implement findKey

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      results = array.slice(0, i);
+      return results;
+    }
+  }
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const data3 = ["I", "will", "test", "this", ",", "I", "will", ",", "test", "that"];
+const results3 = takeUntil(data3, x => x === ',');
+console.log(results3);
+
+assertArraysEqual(results1, data1.slice(0, 5));
+assertArraysEqual(results2, data2.slice(0, 4));
+assertArraysEqual(results3, ["I", "will", "test", "this"]);
