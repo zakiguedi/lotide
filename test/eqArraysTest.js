60c26625c6770164a2fe5aaf2aad60dbeb81d467
zakiguedi <zaki.guedi@gmail.com>
2023-02-14 13:14:25 -0500

    refactoring eqArrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
