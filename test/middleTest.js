b2844ab7aff4f172f60c049fc76d8f91df1c341c
zakiguedi <zaki.guedi@gmail.com>
2023-02-14 16:12:56 -0500

    refactoring tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,18 @@
-const middle = require('../middle');
+/*const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
-
-
 assertArraysEqual(middle([1, 2, 3, 4]),[2,3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4]*/
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+describe("#middle", () => {
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,20 @@
-const tail = require('../tail');
+/*const tail = require('../tail');
 const assertEqual = require('../assertEqual');
-
 const randomArray = ["hello", "Lighthouse", "Labs"];
 assertEqual(randomArray.length, 3);
-assertEqual(tail(randomArray).length, 2);
+assertEqual(tail(randomArray).length, 2);*/
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail([2, 5]), [5]); 
+  });
+
+});
